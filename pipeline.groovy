79af40583bc5f0d459ef605f5feb557152c23565
Amrita Matta <matta.amrita@gmail.com>
2024-11-20 18:37:43 +0200

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,6 +2,11 @@ pipeline {
     agent any
 
     stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/a-matta/testing_demo_playwright_python.git'
+            }
+        }
         stage('Hello') {
             steps {
                 echo 'Hello World'
